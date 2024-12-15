6d9eca3ff383eefd81634d7fc9940cef11f38ce8
AlfVidar <alfvi@AlfHP>
2024-12-15 19:26:04 +0100

    improved plotting
    Former-commit-id: e7febb3b37e2307b5f368d43730a0247ba148490
    Former-commit-id: 070606b5bc63c698f9cc16da04b0d41ce3e88869

--- sim/TB_bandgap/tran.py
+++ sim/TB_bandgap/tran.py
@@ -4,48 +4,38 @@ import yaml
 import matplotlib.pyplot as plt
 import numpy as np
 
-from plot import rawplot
+from plot import *
 
-def plotTempVref(yamlfile):
+def calcPpm(yamlfile):
   # Read result yaml file
   with open(yamlfile + ".yaml") as fi:
     obj = yaml.safe_load(fi)
 
   vref = np.array([])
   temp = np.array([])
-
+  
+  
   for key in obj:
     if key.startswith("vref"):
       vref = np.append(vref, obj[key])
       temp = np.append(temp, int(key.split("_")[1]))
+  
+  print(vref)
+  print(temp)
 
-  sorted_keys = np.argsort(temp)
-  sorted_temp = temp[sorted_keys]
-  sorted_vref = vref[sorted_keys]
-
-
-  fig,ax = plt.subplots(figsize=(10,5))
-  ax.set_xlabel("Temperature [C]")
-  ax.set_ylabel("Voltage [V]")
-  ax.plot(sorted_temp,sorted_vref,label="Vref")
-  ax.grid()
-  ax.legend()
-  ax.set_title("Vref vs Temperature")
-  plt.tight_layout()
-  plt.show()
-
-  # Save new yaml file
-  # with open(yamlfile,"w") as fo:
-  #   yaml.dump(obj,fo)
-
+  ppm = round(((np.max(vref) - np.min(vref))/(np.mean(vref)*(np.max(temp)-np.min(temp)))) * 1e6, 2)
+  print(yamlfile + " Temperature constant: ",ppm, "ppm/C")
+  return ppm
 
 
 def main(name):
 
-  #rawplot(name + ".raw","time","v(vref),v(vn),v(vp)",ptype="same",fname=name + ".pdf")
-
-  #plotTempVref(name)
+  rawplot(name + ".raw",'time',"v(vref),v(xdut.vn),v(xdut.vp)",ptype="same",fname=name + ".pdf", removeFirstSamples=True)
+  #plotTempDependence(name)
+  #calcPpm(name)
+  
+  
   return
 
 
-main("output_tran_temp/tran_SchGtKttTtVt")
+main("tran_etc_temp/tran_SchGtKsfTtVh_20")
